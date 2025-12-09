ab26ced02ba271e11ddd22c544475b345db5179a
GeneralYing <2964311348@qq.com>
2025-12-09 10:04:12 +0800

    wtf

---
+++ 5.cpp
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+
+int f(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+	else if(n==2)
+	{
+		return 2;
+	}
+	else
+	{
+		return f(n - 1) + f(n - 2);
+
+	}
+}
+
+int main()
+{
+	int n;
+	printf("Please enter n:\n");
+	scanf_s("%d", &n);
+	printf("%d", f(n));
+	return 0;
+}
